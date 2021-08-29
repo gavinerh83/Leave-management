2561a96288f7276c245c795955ad908ac7c2be8d
Gavin Erh <gavinerh83@gmail.com>
2021-08-29 14:34:50 +0800

    Add project files.

---
+++ leave-management/Contracts/ILeaveTypeRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using leave_management.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Contracts
+{
+    interface ILeaveTypeRepository : IRepositoryBase<LeaveType>
+    {
+        ICollection<LeaveType> GetEmployeeByLeaveType(int id);
+    }
+}
---
+++ leave-management/Repository/LeaveTypeRepository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using leave_management.Contracts;
+using System.Linq;
+using System.Threading.Tasks;
+using leave_management.Data;
+
+namespace leave_management.Repository
+{
+    public class LeaveTypeRepository : ILeaveTypeRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public LeaveTypeRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public bool Create(LeaveType obj)
+        {
+            _db.LeaveTypes.Add(obj);
+            return Save();
+        }
+
+        public bool Delete(LeaveType entity)
+        {
+            _db.LeaveTypes.Remove(entity);
+            return Save();
+        }
+
+        public ICollection<LeaveType> findAll()
+        {
+            return _db.LeaveTypes.ToList();
+        }
+
+        public LeaveType FindById(int id)
+        {
+            var leaveType = _db.LeaveTypes.Find(id);
+            return leaveType;
+        }
+
+        public ICollection<LeaveType> GetEmployeeByLeaveType(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            return _db.SaveChanges() > 0; //returns an integer that is always greater than 0
+        }
+
+        public bool Update(LeaveType entity)
+        {
+            _db.LeaveTypes.Update(entity);
+            return Save();
+        }
+    }
+}
